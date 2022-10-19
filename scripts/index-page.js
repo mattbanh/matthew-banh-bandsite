ef07b83a250016c3c9781c9c3f462b70195ae242
Matthew Banh <mattbanh92@gmail.com>
2022-10-18 23:14:43 -0700

    created comments form and shows page

---
+++ scripts/index-page.js
@@ -0,0 +1,3 @@
+// You must have an array in JavaScript with 3 default comment objects to start. Comments must have a name, a timestamp, and the comment text.
+// You must have a function called displayComment() that takes in one comment object as a parameter and displays it on the page using JavaScript DOM manipulation.
+// No template literals should be used. All dynamic HTML should be added to DOM via DOM Methods for individual elements. Avoid bulk assigning stringified HTML using innerHTML
